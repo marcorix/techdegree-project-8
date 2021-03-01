b01c8165039fea396fb5643cfd86cf15004a83cd
Marco Ricci <pumavizioso@gmail.com>
2021-03-01 19:57:25 +0000

    first completition

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const urlAPI =
   "https://randomuser.me/api/?results=12&inc=name,gender,picture,email,location,phone,dob&noinfo&nat=US";
 const container = document.querySelector(".grid-container");
-const emplyees = [];
+const modal = document.querySelector(".modal");
+const modalData = document.querySelector(".modal-data");
+const modalClose = document.querySelector(".close-btn");
+const form = document.querySelector(".searchForm");
+const input = document.querySelector(".searchInput");
+
+let employees = [];
 
 // get data from API
 fetch(urlAPI)
@@ -13,24 +19,88 @@ fetch(urlAPI)
 // create HTML from the data
 function displayEmployees(data) {
   employees = data;
-  employeeHTML = ``;
+  let employeeHTML = ``;
 
-  employees.forEach((person, index) => {
+  data.forEach((person, index) => {
     let name = person.name;
     let email = person.email;
     let city = person.location.city;
     let picture = person.picture;
-    console.log(person);
+
     employeeHTML += `
       <div class="card" data-index="${index}">
-      <img src="${picture.medium}" class="avatar" />
+        <img src="${picture.large}" class="avatar" />
         <div class="text-container">
           <h2>${name.first} ${name.last}</h2>
           <p>${email}</p>
           <p>${city}</p>
-      </div>
+        </div>
       </div>
     `;
     container.innerHTML = employeeHTML;
   });
 }
+
+// create HTML for the Modal window
+
+function displayModal(index) {
+  let {
+    email,
+    dob,
+    location: { city, country, postcode, street },
+    name,
+    phone,
+    picture,
+  } = employees[index];
+  console.log(employees);
+  let month = new Date(dob.date).getMonth();
+  let day = new Date(dob.date).getDate();
+  let year = new Date(dob.date).getFullYear();
+
+  const modalHTML = `
+  <img src="${picture.large}" />
+  <h2>${name.first}</h2>
+  <p>${email}</p>
+  <p>${city}</p>
+
+  <hr />
+
+  <p>${phone}</p>
+  <p>${street.number} ${street.name} ${postcode} ${country}</p>
+  <p>Birthday: ${month}/${day}/${year}</p>
+  `;
+  modalData.innerHTML = modalHTML;
+  modal.style.display = "block";
+}
+
+// Filter handler
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const employeesCards = document.querySelectorAll(".card");
+  const nameInput = input.value;
+  employeesCards.forEach((employee) => {
+    const cardName = employee.querySelector("h2");
+    console.log(cardName);
+  });
+});
+
+// Open modal handler
+container.addEventListener("click", (e) => {
+  if (e.target !== container) {
+    const card = e.target.closest(".card");
+    const index = card.getAttribute("data-index");
+    displayModal(index);
+  }
+});
+
+// Close modal handlers
+
+modalClose.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+modal.addEventListener("click", (e) => {
+  if (e.target.className === "modal") {
+    modal.style.display = "none";
+  }
+});
